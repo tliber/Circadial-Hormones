85d21380b58ab476dbc8bb67f1e5020a43930a33
Tole <Tole>
2015-05-07 18:12:45 -0700

    removed non-op file

--- data.js
+++
@@ -1,6 +0,0 @@
-var Data = {};
-Data.Test = {'2h' : 20, '6h' : 22, '10h' : 20, '14h': 18, '18h' : 13, '22h' : 16 };
-Data.Test.units = "mnol/l"
-Data.Cort = {'2h' : 3, '6h' : 9, '10h' : 16, '14h': 8, '18h' : 9, '22h' : 3 };
-Data.Cort.units = "mg/dl"
-Data.Temp = {'2h' : 36, '6h' : 36.5, '10h' : 36.75, '14h': 37, '18h' : 37.1, '22h' : 37.25 }; 
